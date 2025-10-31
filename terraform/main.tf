ba4572c3d3c0398c880b8af27d68f39322845f65
Alresh02 <alreshlal@gmail.com>
2025-10-31 19:49:09 +0530

    made some changes in terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "uptime_ec2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_key_pair" "uptime_key" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_security_group" "uptime_sg" {
+  name        = "uptime-monitor-sg"
+  description = "Allow SSH, HTTP, and monitoring ports"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip_cidr]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "uptime_instance" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.uptime_key.key_name
+  vpc_security_group_ids = [aws_security_group.uptime_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -15,11 +72,9 @@ resource "aws_instance" "uptime_ec2" {
               systemctl enable docker
               EOF
 
-  tags = {
-    Name = "UptimeMonitor"
-  }
+  tags = { Name = "uptime-monitor" }
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.uptime_ec2.public_ip
+output "instance_public_ip" {
+  value = aws_instance.uptime_instance.public_ip
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,5 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "key_name" {
-  description = "EC2 key pair name (create or provide in AWS console)"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "AMI ID to use (Ubuntu 22.04 LTS example)"
-  type        = string
-  default     = "ami-0c55b159cbfafe1f0"
-}
+variable "aws_region"      { default = "ap-south-1" }
+variable "instance_type"   { default = "t3.micro" }
+variable "key_name"        { description = "terraform-key" }
+variable "public_key_path" { description = "Path to your .pub file" }
+variable "my_ip_cidr"      { default = "0.0.0.0/0" }
